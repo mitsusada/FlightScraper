e061354f2b1527501bd91fb589eaae8294329297
nkimoto <kimoppy126@gmail.com>
2018-12-08 14:00:34 +0900

    Add CPA scraping, change to execute all spider per run

--- FlightScraper/app/tasks.py
+++ FlightScraper/app/tasks.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-from .celery_tasks import app
+from .celery import app
 import psycopg2
 from psycopg2.extras import DictCursor
 from logzero import logger
@@ -33,8 +33,9 @@ def main():
     print("Run Process!!!!!")
     conn = get_conn(URI)
     res_dicts = get_resultdict_set(conn)
-    no_data_dicts = [res for res in res_dicts if not all(res.values())]
-    for data_dict in no_data_dicts:
+    # no_data_dicts = [res for res in res_dicts if not all(res.values())]
+    # for data_dict in no_data_dicts:
+    for data_dict in res_dicts:
         cargo_number = str(data_dict['cargo_number'])
         if cargo_number.startswith('933'):
             process.crawl("nca", ID=cargo_number[3:])
@@ -44,6 +45,8 @@ def main():
             process.crawl("cal", ID=cargo_number[3:])
         elif cargo_number.startswith('131'):
             process.crawl("jal", ID=cargo_number[3:])
+        elif cargo_number.startswith('160'):
+            process.crawl("cpa", ID=cargo_number[3:])
         else:
             logger.info("Unexpected prefix...")
     process.start()
